5070410cb12de41ba090d9ad52321393be963fe4
Miquel De Cáceres <miquelcaceres@gmail.com>
2018-05-16 19:54:53 +0200

    udpate individual2species

--- R/individual2species.R
+++ R/individual2species.R
@@ -1,8 +1,11 @@
 #' Transform individual data to species data
 #'
+#' Transforms individual data (plant heights and diameters) into species level data (average height and cover).
+#'
 #' @param x data frame with columns 'plot', 'species', 'H' (height in cm), 'D1' and 'D2' (in cm)
 #' @param sampledarea sampled area in squared meters
 #' @param maxcover maximum allowed cover (set to NA to avoid truncation)
+#' @param na.rm whether to exclude missing values when averaging heights and adding areas
 #'
 #' @return data frame with columns 'plot', 'species', 'H' (mean height in cm), 'C' (cover in percent)
 #' @export
@@ -16,7 +19,7 @@
 #' x = data.frame(plot, species, H, D1, D2)
 #'
 #' individual2species(x, sampledarea = 2)
-individual2species<-function(x, sampledarea = 20, maxcover = 100) {
+individual2species<-function(x, sampledarea = 20, maxcover = 100, na.rm = TRUE) {
   x = as.data.frame(x)
   vars = names(x)
   if(!("plot" %in% vars)) stop("Variable 'plot' needed in 'x'")
@@ -26,16 +29,16 @@ individual2species<-function(x, sampledarea = 20, maxcover = 100) {
   if(!("D2" %in% vars)) stop("Variable 'D2' needed in 'x'")
 
 
-  plots = unique(x$plot)
+  plots = levels(as.factor(x$plot))
   shspdata = NULL
   for(i in 1:length(plots)) {
-    pc = x[x$plot==plots[i],]
+    pc = x[as.character(x$plot)==plots[i],]
     area = pi*((pc$D1/200)*(pc$D2/200)) #area in m2
     species = as.factor(as.character(pc$species))
-    shpc = data.frame(plot = rep(plots[i], length(species)),
-                      species = species,
-                      H = tapply(pc$H, species, FUN=mean, na.rm=T),
-                      C = 100*tapply(area, species, FUN=sum)/sampledarea)
+    shpc = data.frame(plot = rep(plots[i], length(levels(species))),
+                      species = levels(species),
+                      H = tapply(pc$H, species, FUN=mean, na.rm=na.rm),
+                      C = 100*tapply(area, species, FUN=sum, na.rm=na.rm)/sampledarea)
     row.names(shpc) = NULL
     if(!is.null(shspdata)) shspdata = rbind(shspdata, shpc)
     else shspdata = shpc
