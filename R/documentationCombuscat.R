a6d5b3aef61b888d0b2ec1ad9230272f3516d067
Miquel De Cáceres <miquelcaceres@gmail.com>
2018-05-19 15:58:48 +0200

    parameter data doc

---
+++ R/documentationCombuscat.R
@@ -0,0 +1,23 @@
+#' Allometric and bulk density parameter set from project COMBUSCAT
+#'
+#' Description of the aims of the project and funding.
+#'
+#' The data set includes seven data items:
+#'
+#' \itemize{
+#'  \item sp_params_area Species-specific parameter for the relation between height (in cm) and area (in cm2)
+#'  \item sp_params_fine Species-specific allometric parameters for the relation between volume (in m3) and dry weight (in kg) of fine fuels
+#'  \item sp_params_total Species-specific allometric parameters for the relation between volume (in m3) and dry weight (in kg) of total fuels
+#'  \item group_params_area Group-specific parameter for the relation between height (in cm) and area (in cm2)
+#'  \item group_params_fine Group-specific allometric parameters for the relation between volume (in m3) and dry weight (in kg) of fine fuels
+#'  \item group_params_total Group-specific allometric parameters for the relation between volume (in m3) and dry weight (in kg) of total fuels
+#'  \item species_groups Lookup table to match species with functional groups (for species without calibrated parameters).
+#' }
+#'
+#' @name combuscat
+#' @aliases group_params_area group_params_fine group_params_total sp_params_area sp_params_fine sp_params_total species_groups
+#' @docType data
+#' @author Parameter set derived from project COMBUSCAT.
+#' @keywords data
+#' @references Project reference.
+NULL
