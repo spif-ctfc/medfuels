3ae0c73636e3217f139a329a00d8b997522a75ae
miquelcaceres <miquelcaceres@gmail.com>
2018-05-19 20:58:41 +0200

    draw allometry 2

--- R/drawallometry.R
+++ R/drawallometry.R
@@ -8,7 +8,7 @@
 #'
 #' @examples
 #' 
-drawallometry<-function(species, type="total") {
+drawallometry<-function(species, type="total", log = "") {
   if(type=="total") {
     sp_params = get("sp_params_total")
     group_params = get("group_params_total")
@@ -18,7 +18,18 @@ drawallometry<-function(species, type="total") {
   }
   vol = seq(sp_params[species,"minVol"], sp_params[species,"maxVol"],length.out = 100)
   weight = sp_params[species,"a"]*vol^sp_params[species,"b"]
-  df = data.frame(volume = vol, weight = weight)
-  ggplot(df, aes(volume, weight)) +
-    geom_line()
+  vars = (weight^2)*sp_params[species,"gamma_disp"]
+  rate = weight/vars
+  shape = weight * rate
+  q05 = qgamma(0.05, shape, rate)
+  q95 = qgamma(0.95, shape, rate)
+  df = data.frame(volume = vol, weight = weight, q05 = q05, q95 = q95)
+  h <- ggplot(df, aes(volume, weight)) +
+    geom_ribbon(aes(ymin = q05, ymax = q95), fill="grey70", alpha=0.5) +
+    geom_line() +
+    xlab(label = expression(paste("Volume  ", (m^3))))+
+    ylab(label = expression(paste("Dry weight  ", (kg))))
+  if(log=="x" || log=="xy") h <- h+ scale_x_continuous(trans="log10")
+  if(log=="y" || log=="xy") h <- h+ scale_y_continuous(trans="log10")
+  h
 }
