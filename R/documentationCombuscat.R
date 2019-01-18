4062e01f44b492ff633c692f30669d8770e48193
Miquel De Cáceres <miquelcaceres@gmail.com>
2019-01-18 18:04:26 +0100

    individual data for allometries added

--- R/documentationCombuscat.R
+++ R/documentationCombuscat.R
@@ -5,6 +5,7 @@
 #' The data set includes seven data items:
 #'
 #' \itemize{
+#'  \item individual_data Individual-based data underlying the development of allometric equations
 #'  \item sp_params_area Species-specific parameter for the relation between height (in cm) and area (in cm2)
 #'  \item sp_params_fine Species-specific allometric parameters for the relation between volume (in m3) and dry weight (in kg) of fine fuels
 #'  \item sp_params_total Species-specific allometric parameters for the relation between volume (in m3) and dry weight (in kg) of total fuels
@@ -14,7 +15,7 @@
 #' }
 #'
 #' @name combuscat
-#' @aliases group_params_area group_params_fine group_params_total sp_params_area sp_params_fine sp_params_total
+#' @aliases individual_data, group_params_area group_params_fine group_params_total sp_params_area sp_params_fine sp_params_total
 #' @docType data
 #' @author Parameter set derived from project COMBUSCAT.
 #' @keywords data
